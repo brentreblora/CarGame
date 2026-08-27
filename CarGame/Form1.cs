f426b93e091fd7d0e67073d89e20865f327cdb8e
brent_tobias <brentreblora@gmail.com>
2026-08-27 14:49:13 +0800

    Add Game Mechanic Logic

--- CarGame/Form1.cs
+++ CarGame/Form1.cs
@@ -255,6 +255,26 @@ namespace CarGame
         private void SpawnEnemy()
         {
             SpawnInLane(rnd.Next(4), -enemyHeight);
+
+            int[] pattern;
+            int km = (int)(totalDistanceMeters / 1000);
+
+            if (km <= 1)
+                SpawnInLane(rnd.Next(4), -enemyHeight);
+            else if (km <= 3)
+            {
+                pattern = trafficPatterns[rnd.Next(8)];
+                foreach (int lane in pattern)
+                    SpawnInLane(lane, -enemyHeight);
+            }
+
+            else
+            {
+                pattern = trafficPatterns[trafficPatterns.Length];
+                foreach (int lane in pattern)
+                    SpawnInLane(lane, -enemyHeight);
+            }
+
         }
 
         private void SpawnInLane(int lane, float y)
@@ -541,14 +561,12 @@ namespace CarGame
                 DrawCarSelection(e.Graphics);
             else
                 DrawPlayer(e.Graphics);
-
             if (!choosingCar)
                 DrawHUD(e.Graphics);
-
             if (gameOver)
                 DrawGameOver(e.Graphics);
 
-            DrawDebugInfo(e.Graphics);
+            //DrawDebugInfo(e.Graphics);
         }
 
 
@@ -699,7 +717,6 @@ namespace CarGame
 
             //SPEED
             DrawHudPanel(g, new Rectangle(10, 10, 120, 55));
-
             using (Font big = new Font("Segoe UI", 22, FontStyle.Bold))
             using (Font small = new Font("Segoe UI", 10))
             {
@@ -761,10 +778,8 @@ namespace CarGame
                     ClientSize.Width - 100,
                     70);
             }
-
-
-
         }
+
 
         private void DrawHudPanel(Graphics g, Rectangle rect)
         {
@@ -789,6 +804,7 @@ namespace CarGame
 
             path.Dispose();
         }
+
 
 
     }
